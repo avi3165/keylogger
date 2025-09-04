84793919db24089365a8c03a72174f0235b256ae
hagay kohelet <hagay2506@gmail.com>
2025-09-04 15:32:18 +0300

    stam

--- Bacekend/analyze_files.py
+++ Bacekend/analyze_files.py
@@ -24,30 +24,21 @@ def read_text(folder_name,f_date,t_date,date_format = "%Y-%m-%d"):
     f_date = datetime.strptime(f_date,date_format)
     t_date = datetime.strptime(t_date,date_format)
     all_texts = []
-    print(os.getcwd())
-    if folder_name in os.listdir(folder_name) :
-        for file_name in os.listdir(folder_name):
-            if file_name.endswith(".txt"):
-                new_name = file_name[:-4]
-                try:
-                    file_date = datetime.strptime(new_name,date_format)
-                    if f_date <= file_date<= t_date:
-                        file_path = os.path.join(folder_name,file_name)
-                        with open(file_path,"r") as f:
-                            text = f.read()
-                            all_texts.append(text)
-                except ValueError:
-                    continue
-    if not all_texts:
-        result = {
-        "machine_name": folder_name,
-        "content": "NO DATA"
-        }
-        return json.dumps(result) 
+
+    for file_name in os.listdir(folder_name):
+        if file_name.endswith(".txt"):
+            new_name = file_name[:-4]
+            try:
+                file_date = datetime.strptime(new_name,date_format)
+                if f_date <= file_date<= t_date:
+                    file_path = os.path.join(folder_name,file_name)
+                    with open(file_path,"r") as f:
+                        text = f.read()
+                        all_texts.append(text)
+            except ValueError:
+                continue
     result = {
         "machine_name": folder_name,
         "content": all_texts
     }
     return json.dumps(result)
-
-read_text("nn","2025-09-04","2025-09-04")
